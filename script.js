62d9e40fef6be3709bded5313d367cf73234eb87
Federico.Shih <fedeshih@gmail.com>
2018-07-05 20:57:32 -0300

    css and html complete

--- script.js
+++ script.js
@@ -2,17 +2,36 @@ window.onload = function(){
     var elements = document.getElementsByClassName("button");
 
     for(var i = 0; i< elements.length; i++){
-        elements[i].addEventListener("mousemove", mouseHovered);
-        elements[i].addEventListener("mouseout", mouseHovered);
+        elements[i].addEventListener("mousemove", mouseHover);
+        elements[i].addEventListener("mouseout", mouseunHover);
+        elements[i].addEventListener("mousedown", mouseOnClick);
+        elements[i].addEventListener("mouseup", mouseUnClick);
 
     }
+
+    var button0 = document.getElementsByClassName("button0");
+    button0[0].addEventListener("mousemove", mouseHover);
+    button0[0].addEventListener("mouseout", mouseunHover);
+    button0[0].addEventListener("mousedown", mouseOnClick);
+    button0[0].addEventListener("mouseup", mouseUnClick);
 }
 
-function mouseHovered(e){
+function mouseHover(e){
     e.target.style.borderWidth = "2px";
+    e.target.style.cursor ="pointer";
+    e.target.style.userSelect = "none";
+
 }
 
 function mouseunHover(e){
     e.target.style.borderWidth = "0px";
+    e.target.style.opacity = "1";
 }
 
+function mouseOnClick(e){
+    e.target.style.opacity = "0.7";
+}
+
+function mouseUnClick(e){
+    e.target.style.opacity = "1";
+}
